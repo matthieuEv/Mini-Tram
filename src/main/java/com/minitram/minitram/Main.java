5ed608a292bf2422e620087dbfc92c21d462a873
LAPTOP-A6EHR0CC\julie <officielminecraftjuju@gmail.com>
2022-10-12 13:43:46 +0200

    Clean up repot project

--- src/main/java/com/minitram/minitram/Main.java
+++ src/main/java/com/minitram/minitram/Main.java
@@ -1,17 +1,18 @@
 package com.minitram.minitram;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        GridPane grid = new GridPane();
+
+        Scene scene = new Scene(grid, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.minitram.minitram {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.minitram.minitram to javafx.fxml;
-    exports com.minitram.minitram;
-}
