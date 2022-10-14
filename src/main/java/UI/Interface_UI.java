fadd5768368b1c32a7b381d4bc87211647d68e55
LAPTOP-A6EHR0CC\julie <officielminecraftjuju@gmail.com>
2022-10-14 14:25:44 +0200

    Reel fixed size

--- src/main/java/UI/Interface_UI.java
+++ src/main/java/UI/Interface_UI.java
@@ -7,24 +7,25 @@ import javafx.stage.Stage;
 public class Interface_UI {
     protected Stage stage;
     protected double WIDTH = 1000;
-    protected double HEIGHT = 800;
+    protected double HEIGHT = 700;
     public Interface_UI(Stage stage) {
         this.stage = stage;
 
+
+        Scene scene = new Scene(new Game_UI(this), WIDTH, HEIGHT);
+        stage.setScene(scene);
         stage.setTitle("Mini Tram");
         //stage.setMaximized(true);
         stage.show();
-        Scene scene = new Scene(new Menu_UI(this), WIDTH, HEIGHT);
-        stage.setScene(scene);
     }
 
     public double getWIDTH() {
-        WIDTH = stage.getWidth();
+        //WIDTH = stage.getWidth();
         return WIDTH;
     }
 
     public double getHEIGHT() {
-        HEIGHT = stage.getHeight();
+        //HEIGHT = stage.getHeight();
         return HEIGHT;
     }
 }
