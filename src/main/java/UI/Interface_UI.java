0e249c081c031905cc10dfd968f5b04d0760009c
LAPTOP-A6EHR0CC\julie <officielminecraftjuju@gmail.com>
2022-10-14 15:35:45 +0200

    edit interface

--- src/main/java/UI/Interface_UI.java
+++ src/main/java/UI/Interface_UI.java
@@ -5,18 +5,30 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Interface_UI {
-    protected Stage stage;
-    protected double WIDTH = 1000;
-    protected double HEIGHT = 700;
-    public Interface_UI(Stage stage) {
-        this.stage = stage;
+    private static volatile Interface_UI instance = null;
+    private Stage stage;
+    private double WIDTH = 1000;
+    private double HEIGHT = 700;
+    private Game_UI game_ui;
+    private End_UI end_ui;
+    private Menu_UI menu_ui;
 
+    private Interface_UI(Stage stage) {
+        super();
+        this.stage = stage;
 
         Scene scene = new Scene(new Game_UI(this), WIDTH, HEIGHT);
         stage.setScene(scene);
         stage.setTitle("Mini Tram");
         //stage.setMaximized(true);
         stage.show();
+    }
+
+    public static Interface_UI getInstance(Stage stage) {
+        if (instance == null) {
+            instance = new Interface_UI(stage);
+        }
+        return instance;
     }
 
     public double getWIDTH() {
